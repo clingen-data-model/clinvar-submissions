ce6fab2f1f0a714ec8fa37cfc1469920fc40d996
Kyle Ferriter <kferrite@broadinstitute.org>
2020-08-17 15:09:43 -0400

    Refactor more key generator functions. Use CLINVAR_SUBMISSIONS_DB_DIR to override rocksdb directory. Fix combined output message construction

--- src/clinvar_submissions/core.clj
+++ src/clinvar_submissions/core.clj
@@ -4,7 +4,10 @@
             [jackdaw.serdes :as j-serde]
             [cheshire.core :as json]
             [taoensso.timbre :as log]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.string :as s])
+  (:import [org.apache.kafka.streams KafkaStreams]
+           [java.util Properties])
    (:gen-class))
 
 (def app-config {:kafka-host     "pkc-4yyd6.us-east1.gcp.confluent.cloud:9092"
@@ -49,10 +52,12 @@
   "Return true if the message is a clinical assertion
   otherwise return nil"
   [[key v]]
-  (log/trace "in select-clinical-assertion " key)
+;  (log/debug "in select-clinical-assertion " key (get-in (json/parse-string v true) [:content :entity_type]))
   (= "clinical_assertion" (get-in (json/parse-string v true) [:content :entity_type])))
 
-(def rocksdb-path "/tmp/clinvar-submissions-rocksdb/")
+(def rocksdb-path (if (not (empty? (System/getenv "CLINVAR_SUBMISSIONS_DB_DIR")))
+                    (System/getenv "CLINVAR_SUBMISSIONS_DB_DIR")
+                    "/tmp/clinvar-submissions-rocksdb/"))
 (defonce rocksdb (db/init! rocksdb-path))
 
 (def key-functions
@@ -60,8 +65,12 @@
    :rcv_accession (fn [id] (str "rcv_accession-" id))
    :trait_set (fn [id] (str "trait_set-" id))
    :trait (fn [id] (str "trait-" id))
-   :clinical_assertion_observation (fn [id] (str "clinical_assertion_observation-" id))
-
+   :clinical_assertion_observation (fn [id] (s/join "-" ["clinical_assertion_observation" id]))
+   :clinical_assertion (fn [id vcv-id rcv-id] (s/join "-" ["clinical_assertion" vcv-id rcv-id id])) ; SCV last
+   ; combine all fields as the key, assume record is immutable
+   :trait_mapping (fn [scv-id trait-type mapping-type mapping-value mapping-ref medgen-id mapping-name]
+                    (s/join "-" ["trait_mapping" scv-id trait-type mapping-type mapping-value mapping-ref medgen-id mapping-name]))
+   :gene_association (fn [variation-id gene-id] (s/join "-" ["gene_association" variation-id gene-id]))
    })
 
 (defn get-entity-key
@@ -70,20 +79,21 @@
   (let [entity-content (:content entity)
         entity-type (:entity_type entity-content)]
     (case entity-type
-      "trait_mapping" (str entity-type ; TODO combine all fields as the key, record is immutable
-                           "-" (:clinical_assertion_id entity-content)
-                           "-" (:trait_type entity-content)
-                           "-" (:mapping_type entity-content)
-                           "-" (:mapping_value entity-content)
-                           "-" (:mapping_ref entity-content)
-                           "-" (:medgen_id entity-content)
-                           "-" (:mapping_name entity-content))
-      "gene_association" (str entity-type ; association table variation<->gene
-                              "-" (:variation_id entity-content)
-                              "-" (:gene_id entity-content))
-      "clinical_assertion" (str entity-type
-                                 "-" (:variation_archive_id entity-content)
-                                 "-" (:rcv_accession_id entity-content))
+      "trait_mapping" ((:trait_mapping key-functions)
+                        (:clinical_assertion_id entity-content)
+                        (:trait_type entity-content)
+                        (:mapping_type entity-content)
+                        (:mapping_value entity-content)
+                        (:mapping_ref entity-content)
+                        (:medgen_id entity-content)
+                        (:mapping_name entity-content))
+      "gene_association" ((:gene_association key-functions) ; association table variation<->gene
+                           (:variation_id entity-content)
+                           (:gene_id entity-content))
+      "clinical_assertion" ((:clinical_assertion key-functions)
+                             (:id entity-content)
+                             (:variation_archive_id entity-content)
+                             (:rcv_accession_id entity-content))
       (str entity-type "-" (:id entity-content))
       )))
 
@@ -109,7 +119,8 @@
       "update" (db/put-record rocksdb id val)
       "delete" (db/delete-record rocksdb id)
       (ex-info "Message type not [create|update|delete]" {}))
-    ))
+    )
+  [key val])
 
 (defn build-clinical-assertion
   ""
@@ -117,10 +128,11 @@
   ; For the clinical assertion record val, combine all linked entities
   (let [clinical-assertion (json/parse-string val true)
         content (:content clinical-assertion)]
-    (println val)
+    (println "original" val)
     (println "building clinical assertion" (:id content))
-    ; variation archive
-    (let [variation-archive-id (:variation_archive_id content)
+
+    (let [; variation archive
+          variation-archive-id (:variation_archive_id content)
           variation-archive-key ((:variation_archive key-functions) variation-archive-id)
           variation-archive (db/get-key rocksdb variation-archive-key)
 
@@ -135,49 +147,53 @@
           ]
 
       ; return key with string serialized clinical assertion
-      [key
-       (-> clinical-assertion
+      (let [updated-clinical-assertion
+            (-> clinical-assertion ; Add fields to content, remove foreign key fields from content
+                (assoc-in [:content :variation_archive] variation-archive)
+                (update-in [:content] dissoc :variation_archive_id)
 
-            (assoc :variation_archive variation-archive)
-            (dissoc :variation_archive_id)
-
-            (assoc :rcv rcv)
-            (dissoc :rcv_accession_id)
-        )
-       ]
-      ))
+                (assoc-in [:content :rcv] rcv)
+                (update-in [:content] dissoc :rcv_accession_id)
+                )
+            ]
+        (println "updated" updated-clinical-assertion)
+        [key (json/generate-string updated-clinical-assertion)]
+      )))
   )
 
 
 (defn topology [builder in-topic out-topic]
-    (-> (j/kstream builder in-topic)
-        ;; Parse to map and stash data in RocksDB
-        (j/peek to-rocksdb)
-        ; Convert value to map
-        ;(j/map (fn [[k v]] [k (json/parse-string v true)]))
-        ;; Xform clinical assertion using stored data
-        (j/filter select-clinical-assertion)
-;        (j/filter (fn [[k v]] (= "SCV000715423" (-> v :content :id))))
-        (j/map build-clinical-assertion)
-        ; Serialize value back to string
-        (j/map (fn [[k v]] [k (json/generate-string v)]))
-        (j/to out-topic))
-    builder)
+  ""
+  (-> (j/kstream builder in-topic)
+      ;; Parse to map and stash data in RocksDB
+      (j/peek to-rocksdb)
+      ; Convert value to map
+      ;(j/map (fn [[k v]] [k (json/parse-string v true)]))
+      ;; Xform clinical assertion using stored data
+      (j/filter select-clinical-assertion)
+;      (j/filter (fn [[k v]] (= "SCV000105943" (-> (json/parse-string v true) :content :id))))
+      (j/map build-clinical-assertion)
+      ; Serialize value back to string
+;        (j/map (fn [[k v]] [k (json/generate-string v)]))
+      (j/to out-topic))
+  )
 
 (defn write-cfg-to-file
-  [kafka-config]
-  (with-open [writer (io/writer "kafka.properties")]
+  [kafka-config filename]
+  (with-open [writer (io/writer filename)]
     (doseq [[k v] kafka-config]
       (.write writer (str k "=" v "\n")))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Construct topology and start kafka streams application"
   [& args]
-  (write-cfg-to-file (kafka-config app-config))
+  (write-cfg-to-file (kafka-config app-config) "kafka.properties")
   (log/set-level! :info)
-  (let [builder (topology (j/streams-builder) (:input topic-metadata) (:output topic-metadata))
-        app (j/kafka-streams builder (kafka-config app-config))]
-    (j/start app)))
+
+  (let [builder (j/streams-builder)]
+    (topology builder (:input topic-metadata) (:output topic-metadata))
+    (let [app (j/kafka-streams builder (kafka-config app-config))]
+    (j/start app))))
 
 
 
